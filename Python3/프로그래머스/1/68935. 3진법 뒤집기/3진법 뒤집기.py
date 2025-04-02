daf82e3c591af8c2c6479aa8215b6cc84c71453e
imdadadabong <hellodadakim@gmail.com>
2025-04-02 09:25:17 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 9.42 MB -BaekjoonHub

--- Python3/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ Python3/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,12 +1,3 @@
 def solution(n):
-    
-    answer = ''
-    while n > 0:
-        n, re = divmod(n, 3)
-        answer += str(re)
-    return int(answer, 3)
-
-#divmod () : 몫과 나머지를 리턴, 리턴 값이 2개이기 때문에 튜플을 사용 
-# int(str, base) : base 진법으로 구성된 str 형식의 수를 10진법으로 변환  
-# 만약 역순인 진수를 뒤집어, 원래 변환하고자 하는 base를 출력하고자 할 때 아래 출력 내용 사용
-# return rev_base[::-1]
+    answer = 0
+    return answer
