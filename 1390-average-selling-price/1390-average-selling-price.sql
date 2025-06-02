1dd28213d3d5df68181ee720960152085e12720f
dayeon kim (aka Stella) <hellodadakim@gmail.com>
2025-06-02 10:15:31 +0900

    Time: 765 ms (84.85%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -5,3 +5,9 @@ LEFT JOIN UnitsSold u
 ON p.product_id = u.product_id AND 
 u.purchase_date BETWEEN p.Start_date and p.end_date
 GROUP BY p.product_id
+
+#해당 Column의 값이 NULL을 반환할 경우 다른 값으로 출력하고 싶다면 INFULL함수를 사용하면 됨 
+# AGE 컬럼이 NULL인 경우, -1을 반환
+#SELECT IFNULL(AGE, -1) FROM 테이블명;
+# ADDRESS 컬럼이 NULL인 경우, '주소 없음'을 반환
+#SELECT IFNULL(ADDRESS, "주소 없음") FROM 테이블명;
