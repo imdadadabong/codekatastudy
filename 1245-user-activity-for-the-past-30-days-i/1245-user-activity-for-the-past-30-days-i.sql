38349ca6db1b98a143dfb3225fc370ca046c5860
dayeon kim (aka Stella) <hellodadakim@gmail.com>
2025-06-05 09:46:38 +0900

    Time: 523 ms (38%), Space: 0B (100%) - LeetHub

--- 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -3,3 +3,7 @@ select activity_date as day, count(DISTINCT user_id) as active_users
 from Activity
 where datediff('2019-07-27', activity_date) <30 and datediff('2019-07-27', activity_date) >=0 
 group by activity_date;
+
+#count(DISTINCT user_id) as active_users : 중복없이 User_id 구분 
+#where datediff('2019-07-27', activity_date) <30 and datediff('2019-07-27', activity_date) >=0   : 30일동안 활성 사용자수 계산 
+#group by activity_date 날짜별 묶어서 집계 
