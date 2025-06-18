5e62093cc1522bf611cff84913ca116532a1458c
dayeon kim (aka Stella) <hellodadakim@gmail.com>
2025-06-18 11:07:18 +0900

    Time: 701 ms (97.8%), Space: 0B (100%) - LeetHub

--- 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
-with ordered as (
-    select * , sum(weight) over (order by turn) as total_weight from Queue)
-
-select person_name
-from ordered
-where total_weight <= 1000
-order by total_weight desc 
-limit 1 ;
+SELECT person_name FROM (
+  SELECT person_name, turn,
+         SUM(weight) OVER (ORDER BY turn) AS total_weight
+  FROM Queue
+) AS queues
+WHERE total_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
